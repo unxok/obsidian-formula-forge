09f6aee54430dfbb270894499b241060727b4ec8
Unxok <me@unxok.com>
2026-06-08 22:47:21 -0700

    refactor: allow passing an element instead of a setting

--- src/utils/obsidian.ts
+++ src/utils/obsidian.ts
@@ -3,19 +3,21 @@ import { TryCatchResult } from "./pure";
 
 type Validate = (validations: TryCatchResult<void>[]) => boolean;
 
-export const validateSetting = (setting: Setting): Validate => {
+export const validateSetting = (setting: Setting | HTMLElement): Validate => {
 	const validationEl = window.createDiv({ cls: "mod-warning" });
 
 	return (validations) => {
 		validationEl.empty();
-		setting.descEl.append(validationEl);
+		const el = setting instanceof Setting ? setting.descEl : setting;
+		el.append(validationEl);
 
-		return validations.every((result) => {
-			if (!result.success) {
-				validationEl.createEl("br");
-				validationEl.appendText(result.error);
-			}
-			return result.success;
-		});
+		return validations.reduce((acc, result) => {
+			if (result.success) return acc;
+
+			validationEl.createEl("br");
+			validationEl.appendText(result.error);
+
+			return false;
+		}, true);
 	};
 };
